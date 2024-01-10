b0e80dc59583a5fee2ca3e758e500584b13932ac
LazarGolomeevUni <444180@student.fontys.nl>
2024-01-10 16:55:15 +0100

    Add CICD to AWS

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { createPool } = require("mysql");
+const { body, validationResult } = require('express-validator');
 
 const app = express();
 
@@ -41,25 +42,31 @@ app.get("/users", (req, res) => {
 });
 
 //Register
-app.post("/register", async (req, res) => {
+app.post("/register", 
+  // Define validation rules
+  body('username').isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+  body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long'),
+  body('age').isNumeric().withMessage('Age must be a number'),
+  body('consent').equals('1').withMessage('Consent is required'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        if (req.body.consent === 0) {
-            res.status(451).send("No consent no account");
-        } else {
-            const sql = `INSERT INTO userdb.users (username, password, age, consent)
+        const sql = `INSERT INTO userdb.users (username, password, age, consent)
                VALUES ('${req.body.username}', '${hashedPassword}', ${req.body.age}, ${req.body.consent})`;
-            pool.query(sql, (err, response) => {
-                if (err) {
-                    res.status(500).send(err);
-                }
-                else {
-                    console.log("result: " + response)
-                    res.status(201).send();
-                }
-            })
-        }
-
+        pool.query(sql, (err, response) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {
+                console.log("result: " + response)
+                res.status(201).send();
+            }
+        })
     } catch {
         res.status(500).send();
     }
